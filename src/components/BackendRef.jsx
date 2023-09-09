47f42fb926615973ab3d73c8e45e3e2ba1cdac8b
sarthakagarwal882 <infosarthakagarwal882@gmail.com>
2023-09-09 20:06:14 +0530

    update backend_ref

--- src/components/BackendRef.jsx
+++ src/components/BackendRef.jsx
@@ -5,5 +5,6 @@ if (import.meta.env.MODE === 'production') {
 else if (import.meta.env.MODE === 'development') {
     backend_ref = import.meta.env.VITE_LOCAL_LINK
 }
+// backend_ref = import.meta.env.VITE_SERVER_LINK
 
 export default backend_ref
